e5b33393ba94fde4b8fb3cec9eca97f819bd3577
yatakeke <yatasunshine.1456@gmail.com>
2023-09-24 14:15:25 +0900

    関数への切り出し

--- python/tennis1.py
+++ python/tennis1.py
@@ -18,27 +18,16 @@ class TennisGame1:
         result = ""
         temp_score = 0
         if self.p1points == self.p2points:
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-            return result
+            return self.score_when_same_point()
 
         if self.p1points >= 4 or self.p2points >= 4:
-            minus_result = self.p1points - self.p2points
-            if minus_result == 1:
-                result = "Advantage player1"
-            elif minus_result == -1:
-                result = "Advantage player2"
-            elif minus_result >= 2:
-                result = "Win for player1"
-            else:
-                result = "Win for player2"
+            return self.score_when_deuce()
 
-            return result
+        return self._score(result)
 
-        for i in range(1,3):
+    def _score(self, result):
+        # MEMO 良い名前がつけられなかったため
+        for i in range(1, 3):
             if i == 1:
                 temp_score = self.p1points
             else:
@@ -50,4 +39,24 @@ class TennisGame1:
                 2: "Thirty",
                 3: "Forty",
             }[temp_score]
+        return result
+
+    def score_when_deuce(self):
+        minus_result = self.p1points - self.p2points
+        if minus_result == 1:
+            result = "Advantage player1"
+        elif minus_result == -1:
+            result = "Advantage player2"
+        elif minus_result >= 2:
+            result = "Win for player1"
+        else:
+            result = "Win for player2"
+        return result
+
+    def score_when_same_point(self):
+        result = {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All",
+        }.get(self.p1points, "Deuce")
         return result
